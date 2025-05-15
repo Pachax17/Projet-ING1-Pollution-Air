60ab9b5d0b29c0a0c92ca19b8365da7cfc38e5ac
PaSssTec29 <athulliez29@gmail.com>
2025-05-15 17:48:47 +0200

    Add files via upload

---
+++ Début.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+pd.options.display.max_rows = 9999
+
+df = pd.read_csv('data.csv', sep=';')
+
+df.drop(columns=['Lien vers les mesures en direct', 'Mesures d’amélioration mises en place ou prévues','air','actions'],inplace=True)
+df.drop_duplicates(inplace=True)
+df.dropna(how='all',inplace=True)
+#print(df.info())
+
+Metro=df[df["Nom de la ligne"].str.contains("Métro", case=False, na=False)]
+Train=Metro.head(int(0.7*len(Metro)+1))
+Test=Metro.tail(int(0.3*len(Metro)))
+#print(Metro.to_string())
+#print(Train.info())
+#print(Test.info())
+
+#Metro.to_csv("stations_metro2.csv", index=False, encoding='utf-8-sig',sep=';')
+#Train.to_csv("stations_metro2.csv", index=False, encoding='utf-8-sig',sep=';')
+#Test.to_csv("stations_metro2.csv", index=False, encoding='utf-8-sig',sep=';')
