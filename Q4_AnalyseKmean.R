0606c11fbac7b2d1c8bdb332eb8b92be7c572ee5
Pachax17 <131616395+Pachax17@users.noreply.github.com>
2025-05-17 02:28:20 +0200

    Maj CSV principale
    Suppression des colonnes répétant la même info sur le niveau de pollution --> combinée en une "pollution_unifiee"

--- Q4_AnalyseKmean.R
+++ Q4_AnalyseKmean.R
@@ -1,9 +1,9 @@
 ## Q4 (jeux de donn?es "stations_metro2.csv")
 
-df <- read.csv("stations_metro2.csv", sep = ";")
+df <- read.csv("stations_metro2.csv", sep = ";", stringsAsFactors = FALSE)
+head(df)
 X <- scale(df[, c("stop_lat", "stop_lon")])
-
-cl <- kmeans(X, centers = 3)
+cl <- kmeans(X, centers = 5)
 df$cluster <- cl$cluster
 
 # Visualiser
